2969c0a54b34d830fc0d63b67abd03d077efafdf
pffffffffffh <2718261451@qq.com>
2024-12-19 19:59:29 +0800

    1

--- leetcode/leetcode/1.h
+++ leetcode/leetcode/1.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <vector>
+#include <unordered_map>
+#include <algorithm>
 #include "tool.h"
 
 using namespace std;
@@ -7,19 +9,39 @@ using namespace std;
 //1.两数之和
 class Solution {
 public:
+    //vector<int> twoSum(vector<int>& nums, int target) {
+    //    vector<int> res;
+
+    //    for (int i = 0; i < nums.size(); i++)
+    //    {
+    //        for (int j = i + 1; j < nums.size(); j++)
+    //        {
+    //            if (nums[i] + nums[j] == target)
+    //            {
+    //                res.push_back(i);
+    //                res.push_back(j);
+    //                return res;
+    //            }
+    //        }
+    //    }
+    //    return res;
+    //}
+
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> res;
+        unordered_map<int, int> map;
 
         for (int i = 0; i < nums.size(); i++)
         {
-            for (int j = i + 1; j < nums.size(); j++)
+            auto it = map.find(target - nums[i]);
+            if (it==map.end()){
+                map[nums[i]] = i;
+            }
+            else
             {
-                if (nums[i] + nums[j] == target)
-                {
-                    res.push_back(i);
-                    res.push_back(j);
-                    return res;
-                }
+                res.push_back(it->second);
+                res.push_back(i);
+                return res;
             }
         }
         return res;
